ad09bb6019154b2dc426d2044b0fdc29ba90ec63
Siaosi Moala <loto_toa@hotmail.com>
2024-04-16 16:33:29 +1000

    DONE

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  document.getElementById("create-task-form").addEventListener("submit", function(event) {
+    event.preventDefault();
+    let task = document.createElement("li");
+    task.innerText = event.target[0].value;
+    document.getElementById("tasks").appendChild(task);
+    event.target[0].value = "";
+  });
 });
